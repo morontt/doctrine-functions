ba23fc722f4b7042ef64e8644fd48cf9c04ace67
Andrey Kryukov <a.kryukov.nor@gmail.com>
2014-10-07 15:00:36 +0800

    add MONTH() function

---
+++ src/Morontt/DQL/DateTime/Month.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Autor: avknor.github.io
+ * Date: 07.10.14
+ * License: MIT
+ */
+
+namespace Rem\SecurityBundle\DQL;
+
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+/**
+ * DQL function for month
+ *
+ * Example: MONTH()
+ */
+class Month extends FunctionNode
+{
+    public $date = null;
+
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        return "MONTH(" . $sqlWalker->walkArithmeticPrimary($this->date) . ")";
+    }
+    
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $this->date = $parser->ArithmeticPrimary();
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }    
+}
